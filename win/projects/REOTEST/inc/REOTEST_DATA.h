003458b797df1b41223b00513e4dfe0e97d772ef
reo.ih <yogan193@mail.ru>
2022-10-10 17:16:33 +0300

    1010

--- win/projects/REOTEST/inc/REOTEST_DATA.h
+++ win/projects/REOTEST/inc/REOTEST_DATA.h
@@ -1,16 +1,43 @@
+#include <time.h>
+
 struct REOTEST_INPUT
 {
-    int a;
-    int b;
+    int AI[60];     // числовые входы
+    int DI[4];      // дискретные входы
 };
 
 struct REOTEST_OUTPUT
 {
-    int sum;
-    int dif;
+    int AO[60];     //  числовые выходы
+    int DO[4];      //  дискретные выходы
 };
 
 struct REOTEST_STATE
 {
-    int ass;
+    int AS[120];    //  числовые переменные состояния
+    int DS[8];       //  дискретные переменные состояния
 };
+
+struct REOTEST_DATA_HEADER
+{
+    char KEY0;
+    char KEY1;
+    char KEY2;
+    char KEY3;
+    int SIZE;
+    short D_YEAR;
+    short D_MONTH;
+    int D_DAY;
+    char T_HOUR;
+    char T_MIN;
+    char checksum;
+    char term;
+};
+
+union REOTEST_DATA_U
+{
+    char c[16];
+    struct REOTEST_DATA_HEADER h;
+};
+
+void REOTEST_DEFAULT_HEADER(union REOTEST_DATA_U* HEADER, int SIZE);
--- win/projects/REOTEST/src/main.c
+++ win/projects/REOTEST/src/main.c
@@ -1,27 +1,100 @@
 #include "../inc/main.h"
+FILE *f_inputs;
+FILE *f_outputs;
 
-void main(void)
+
+union REOTEST_DATA_U h_outputs;
+union REOTEST_DATA_U h_inputs;
+void REOTEST_DEFAULT_HEADER(union REOTEST_DATA_U* HEADER, int SIZE)
 {
-    int i, c, e;
-    c = 0; e = 0;
-    for (i = 1; i<=100; i++)
+    time_t t = time(NULL);
+    struct tm now = *localtime(&t);
+    char checksum;
+    HEADER->h.KEY0 = 'Y';
+    HEADER->h.KEY1 = 'H';
+    HEADER->h.KEY2 = 'W';
+    HEADER->h.KEY3 = 'H';
+    HEADER->h.SIZE      = SIZE;
+    HEADER->h.D_YEAR    = now.tm_year;
+    HEADER->h.D_MONTH   = now.tm_mon;
+    HEADER->h.D_DAY     = now.tm_mday;
+    HEADER->h.T_HOUR    = now.tm_hour;
+    HEADER->h.T_MIN     = now.tm_min;
+    checksum = HEADER->c[0];
+    for (int i=1; i<=17;i++)
     {
-        testset1.a = rand();
-        testset1.b = rand();
-        printf("%d; %d \n", testset1.a, testset1.b);
-        test_update(&testset1, &resset1);
-        if (resset1.sum == (testset1.a + testset1.b))
-        {
-            c++;
-        } else {
-            e++;
-        }
-        if (resset1.dif == (testset1.a - testset1.b))
-        {
-            c++;
-        } else {
-            e++;
-        }
+        checksum ^= HEADER->c[i];
     }
-    printf("correct: %d/200; error: %d/200 \n", c,e);
+    HEADER->h.checksum = checksum;
+    HEADER->h.term = '}';
+};
+
+void main(int argc, char *argv[])
+{
+    char err_input      = 0;
+    char err_output     = 0;
+    char *c;
+    for (int i=0; i<argc; i++)                                              // поиск входного файла в аргументах командной строки
+        {
+            c = argv[i];
+            if ((*c=='-')&&(*(c+1)=='i'))                                       // найдена команда "-i", в следующей будет содержаться имя файла
+            {
+                printf("input file: %s\n",argv[i+1]);
+                if((f_inputs = fopen((char *)argv[i+1], "r"))!= NULL)               // пытаемся открыть файл
+                {
+                    char checksum = (char)0x00;
+                    for (int j=0; j<(sizeof(struct REOTEST_DATA_HEADER)-2);j++)
+                    {
+                        h_inputs.c[j] = (char)getc(f_inputs);
+                        //printf("%c", h_inputs.c[j]);
+                        checksum ^= h_inputs.c[j];
+                    }
+                    h_inputs.c[sizeof(struct REOTEST_DATA_HEADER)-1] = (char)getc(f_inputs);
+                    if(h_inputs.c[sizeof(struct REOTEST_DATA_HEADER)-1] == checksum)
+                    {
+                        //printf("%x = %x, header is ok.\n",checksum, checksum);
+                        getc(f_inputs);
+                    }    else
+                    {
+                        //printf("%x != %x, header is incorrect.\n",checksum, h_inputs.c[sizeof(struct REOTEST_DATA_HEADER)-1]);
+                    }
+                    //printf("\nsize of input:%d\n",h_inputs.h.SIZE);
+                    i = argc+1;
+                } else                                                             
+                {
+                    //printf("Can't open %s\n", argv[i+1]);
+                }
+            }
+        }
+
+    REOTEST_DEFAULT_HEADER(&h_outputs, 0);
+    for (int i=0; i<argc; i++) // поиск входного файла в аргументах командной строки
+        {
+            c = argv[i];
+            //printf("%x : %s : %c\n",c,c,*(c+1));
+            if ((*c=='-')&&(*(c+1)=='o'))
+            {
+                printf("output file: %s.txt\n",argv[i+1]);
+                f_outputs = fopen((char *)argv[i+1], "w");
+                i = argc+1;
+            }
+            if (i==(argc-1))
+            {
+                printf("output file: results.out \n");
+                f_outputs = fopen("results.out", "w");
+            }
+        }
+
+        
+    char checksum = (char)0x00;
+    for (int j=0; j<sizeof(struct REOTEST_DATA_HEADER); j++)
+        {
+            putc(h_outputs.c[j],f_outputs);
+            checksum ^= h_outputs.c[j];
+            //printf("checksum:%x %x\n", h_outputs.c[j], checksum);
+        }
+    putc(checksum, f_outputs);
+    putc('}', f_outputs);
+
+    fclose(f_inputs);   fclose(f_outputs);
 }
