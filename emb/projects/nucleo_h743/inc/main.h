8e80f0208789761c7129d030dc60036db05c13ba
yogan2015 <yogan193@mail.ru>
2022-12-09 18:09:55 +0300

    compile barrier

--- emb/projects/nucleo_h743/inc/main.h
+++ emb/projects/nucleo_h743/inc/main.h
@@ -1,5 +1,13 @@
 #ifdef RELPATH
     #include "stm32h743xx.h"
 #else
-    #include "stm32h743xx.h"
+    #include "../../../cmn/target/stm32f74x/stm32h743xx.h"
+    #include "../../../cmn/target/stm32f74x/system_stm32h7xx.h"
+    #include "../../../cmn/core/mpu_armv7.h"
+
+    #include "../../../cmn/core/core_cm7.h"
+
+    #include "../../../cmn/CMSIS/cmsis_compiler.h"
+    #include "../../../cmn/CMSIS/cmsis_version.h"
+    #include "../../../cmn/CMSIS/cmsis_gcc.h"
 #endif
