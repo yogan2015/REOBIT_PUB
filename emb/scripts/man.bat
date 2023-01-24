bdff6ff1e7f175f440b9282f1481a6363b82d27d
yogan2015 <yogan193@mail.ru>
2023-01-24 11:29:33 +0300

    kucha

--- emb/scripts/man.bat
+++ emb/scripts/man.bat
@@ -1,9 +1,20 @@
-@chcp 1251 >nul
-@cls
-@echo building project "%1"
-@cd .\projects\%1\build
-..\..\..\utils\make\mingw32-make -f .\..\makefile %2 1>scenario.log 2>errors.log
-@del .\*.o
-@del ..\src\*.o
-@del .\*.bin
-@cd ..\..\..
+chcp 1251 >nul
+REM cls
+
+echo off
+
+set /p makepath=<utils\make\make.path
+REM echo %makepath%
+
+echo обработка преокта "%1"...
+IF EXIST .\projects\%1 @(
+    IF NOT EXIST .\projects\%1\build md .\projects\%1\build
+    cd .\projects\%1\build
+    call %makepath% -f .\..\makefile %2 1>scenario.log 2>errors.log
+    del .\*.o
+    del ..\src\*.o
+    del .\*.bin
+    cd ..\..\..
+) else (
+    echo Проект "%1" не найден 
+)
--- emb/utils/toolchain/readme.txt
+++ emb/utils/toolchain/readme.txt
@@ -1,1 +1,6 @@
-В папке bin содержится копия GNU-tools-for-STM32 из набора утилит STM32Cube_CLT
+1. Если на данном устройстве отсутствует файл toolchain.path, создайте его и поместите в каталог "..utils\toolchain"
+2. В файл toolchain.path запишите путь к "...arm-none-eabi\bin" на данном утройстве.
+    формат записи:
+
+ gccpath = C:(\STM32CubeCLT\STM32CubeCLT\GNU-tools-for-STM32)\bin
+
