0c33b8072fe1c03ae9de8b9f847d162aadf09778
yogan2015 <yogan193@mail.ru>
2023-01-25 23:46:19 +0300

    evening wedensday

--- emb/projects/MBS_K1921/main2.c
+++ emb/projects/MBS_K1921/main2.c
@@ -57,6 +57,7 @@ int16 main()
     uint32_t b = 1;
     int16_t  c = 1;
     int32_t  d = 1;
+    DINT;
     Init_CLK();  
     Init_GPIO();
     Init_TIM0();
@@ -68,44 +69,44 @@ int16 main()
         static TMbGetRecord *GetRecord;
 
         static UART_Init_TypeDef *UART0_Init;
-    #define Part1
-    #define Part2
-    #define Part3
-    #ifdef Part1
-        SlaveInfo->DeviceDescr      = 0;
-        SlaveInfo->DeviceInfo       = 0;
-        SlaveInfo->DeviceNumber     = 0;
-        SlaveInfo->SlaveID          = 0;
-        SlaveInfo->VersionDate      = 0;
-        SlaveInfo->VersionNumber    = 0;
-    #endif
-    #ifdef Part2
-        Mb->Params.UartID           = 0;
-        Mb->Params.Mode             = 1; // slave
-        Mb->Params.Slave            = 1;
-        Mb->Params.BaudRate         = 1152;
-        Mb->Params.BrrValue         = 1152;
-        Mb->Params.Parity           = 0;
-        Mb->Params.StopBits         = 1;
-        Mb->Params.UserMode         = 0;
-        Mb->Params.RetryCount       = 0;
-        Mb->Params.Scale            = 4;
-        Mb->Params.RxDelay          = 5;
-        Mb->Params.TxDelay          = 2;
-        Mb->Params.ConnTimeout      = (uint16_t)(3.0*SYSTICK_FREQ);
-        Mb->Params.AckTimeout       = (uint16_t)(1.0*SYSTICK_FREQ);
-        Mb->Params.SlaveInfo        = (TMbSlaveInfo *)&SlaveInfo;
-        Mb->Params.TrEnable         = GetData;
-        Mb->Params.GetData          = GetData;
-        Mb->Params.GetRecord        = 0;
-    #endif
-    #ifdef Part3
-        UART0_Init->UART_DataWidth  = UART_DataWidth_8;
-        UART0_Init->UART_FIFOEn     = DISABLE;
-        UART0_Init->UART_ParityBit  = UART_ParityBit_Odd;
-        UART0_Init->UART_RxEn       = ENABLE;
-        UART0_Init->UART_TxEn       = ENABLE;
-    #endif
+        #define Part1
+        #define Part2
+        #define Part3
+        #ifdef Part1
+            SlaveInfo->DeviceDescr      = 0;
+            SlaveInfo->DeviceInfo       = 0;
+            SlaveInfo->DeviceNumber     = 0;
+            SlaveInfo->SlaveID          = 0;
+            SlaveInfo->VersionDate      = 0;
+            SlaveInfo->VersionNumber    = 0;
+        #endif
+        #ifdef Part2
+            Mb->Params.UartID           = 0;
+            Mb->Params.Mode             = 1; // slave
+            Mb->Params.Slave            = 1;
+            Mb->Params.BaudRate         = 1152;
+            Mb->Params.BrrValue         = 1152;
+            Mb->Params.Parity           = 0;
+            Mb->Params.StopBits         = 1;
+            Mb->Params.UserMode         = 0;
+            Mb->Params.RetryCount       = 0;
+            Mb->Params.Scale            = 4;
+            Mb->Params.RxDelay          = 5;
+            Mb->Params.TxDelay          = 2;
+            Mb->Params.ConnTimeout      = (uint16_t)(3.0*SYSTICK_FREQ);
+            Mb->Params.AckTimeout       = (uint16_t)(1.0*SYSTICK_FREQ);
+            Mb->Params.SlaveInfo        = (TMbSlaveInfo *)&SlaveInfo;
+            Mb->Params.TrEnable         = GetData;
+            Mb->Params.GetData          = GetData;
+            Mb->Params.GetRecord        = 0;
+        #endif
+        #ifdef Part3
+            UART0_Init->UART_DataWidth  = UART_DataWidth_8;
+            UART0_Init->UART_FIFOEn     = DISABLE;
+            UART0_Init->UART_ParityBit  = UART_ParityBit_Odd;
+            UART0_Init->UART_RxEn       = ENABLE;
+            UART0_Init->UART_TxEn       = ENABLE;
+        #endif
 
     //UART_Init(NT_UART0, UART0_Init);
     NT_GPIOC->ALTFUNCCLR_bit.ALTFUNCCLR = (1 << 8 );
@@ -116,6 +117,7 @@ int16 main()
     NT_GPIOC->ALTFUNCCLR_bit.ALTFUNCCLR = (1 << 13);
     NT_GPIOC->OUTENSET_bit.OUTENSET |= (0b111111 << 8);
     NT_GPIOC->DATA |=(0b111111<<8);
+    EINT;
     while (1)
     {
         ;
@@ -125,19 +127,23 @@ int16 main()
 
 void TIM0_IRQHandler (void)
 {
+    //return;
     //phase = (phase < 0)?(5):(phase-1);
     //pulsar(phase);
-    NT_GPIOC->DATA ^= (1<< 8);
-    if (NT_PWM5->TBSTS_bit.CTRDIR == 0)
+    NT_GPIOC->DATA ^= (1 << 9);
+    if (NT_PWM0->TBSTS & PWM_TBSTS_CTRDIR_Msk)
     {
-        NT_GPIOC->DATA |= (1<<9);
+        NT_GPIOC->DATA |= (1<<8);
     } else
     {
-        NT_GPIOC->DATA &=~(1<<9);
+        NT_GPIOC->DATA &=~(1<<8);
     }
-    
-    p = (p<0x10)?(0x1FF0):(p-1);
-    NT_PWM3->CMPA_bit.CMPA = p;
-    NT_PWM3->CMPB_bit.CMPB = p;
     NT_TIMER0->INTSTATUS_INTCLEAR_bit.INT = 1;
+}
+
+extern void EPWM_0_IRQHandler(void)
+{
+    NT_GPIOC->DATA ^= (1<< 13);
+    NT_PWM0->ETCLR |= 0x1;
+    while(1){}
 }
